b0b7047517ce2c40e81b1257c2ec90199f249615
mariahjo <mariahjo@kth.se>
2015-02-08 15:48:41 +0100

    pill

--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -1,5 +1,6 @@
 //ExampleView Object constructor
 var ExampleView = function (container,model) {
+
 	
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
@@ -10,15 +11,21 @@ var ExampleView = function (container,model) {
 	this.dishName = container.find("#dishName");
 	this.dishPrice = container.find("#dishPrice");
 	this.allDishes = container.find("#allDishes");
-	
+
 	this.numberOfGuests.html(model.getNumberOfGuests);
 	this.totalCost.html(model.getNumberOfGuests);
 	this.dishName.html(model.getSelectedDish);
 	this.dishPrice.html(model.getDishPrice);
+	this.allDishes.html(this.getDishes);
+
+	var getDishes=function()
+	{
+		return "hejehehe";
+
+	};
 
 
-	var nameDish= model.getAllDishes('main dish').name//Den här måste skrivas som en for-loop då det kommer plockas ut många namn
+	//Den här måste skrivas som en for-loop då det kommer plockas ut många namn
 //Vi måste sedan göra detsamam för image samt för beskrivning av maträtten
-	this.allDishes.html(nameDish);
 }
  
